6f2ed721c0f0b5caf49b49fb9d3c106eeaba17ba
lucianodornfeld18-creator <lucianodornfeld18@gmail.com>
2026-06-26 17:35:16 -0400

    Set business email to info@lakewoodranchconcretefl.com site-wide

--- _data.py
+++ _data.py
@@ -33,7 +33,7 @@ BUSINESS = {
     "phone": "+19413524308",
     "phone_display": "(941) 352-4308",
     "phone_tel": "+19413524308",
-    "email": "{{EMAIL}}",
+    "email": "info@lakewoodranchconcretefl.com",
     "year_founded": "{{YEAR}}",
     "rating": "{{RATING}}",
     "review_count": "{{REVIEW_COUNT}}",
